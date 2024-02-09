ff35c30d8a49fe71aeb5dbf91aaaad3ce70a612f
kal-dereje <kalabdereje20@gmail.com>
2024-02-09 17:40:01 +0300

    no change

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Login from "./FrontEnd/Login/Login";
 import Header from "./FrontEnd/Home/header";
 import Verification from "./FrontEnd/Signup/VerfifcationPage";
- import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ClientSignup from "./FrontEnd/Signup/ClientSignup";
 import TherapistSignup from "./FrontEnd/Signup/TherapistSignup";
 
@@ -9,18 +9,13 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/ClientSignup" element={<ClientSignup />} />
-      <Route path="/TherapistSignup" element={<TherapistSignup />} />
-      <Route path="/Login" element={<Login />} />
-      <Route path="/Verification" element={<Verification />} />
-        <Route path="/"  element={<Header />}>
-     
-          </Route>
+        <Route path="/ClientSignup" element={<ClientSignup />} />
+        <Route path="/TherapistSignup" element={<TherapistSignup />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Verification" element={<Verification />} />
+        <Route path="/" element={<Header />}></Route>
       </Routes>
-    
     </BrowserRouter>
-   
-    
   );
 }
 
