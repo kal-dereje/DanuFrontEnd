698c1072ee3bb7b3f6258d97e47e6bec116f30e9
unknown <Hilina.mastewal.ue6602@gmail.com>
2024-02-20 14:34:11 +0300

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import Verification from "./FrontEnd/Signup/VerfifcationPage";
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import ClientSignup from "./FrontEnd/Signup/ClientSignup";
 import TherapistSignup from "./FrontEnd/Signup/TherapistSignup";
-import WelcomeFrom from "./FrontEnd/Form/ClientForm/welcomeFrom";
-import QuestionPage from "./FrontEnd/Form/ClientForm/QuestionPage";
-const questions = [
+import ClientWelcomeForm from "./FrontEnd/Form/ClientForm/welcomeFrom";
+import TherapistWelcomeForm from "./FrontEnd/Form/TherapistForm/welcomeForm";
+import ClientQuestionPage from "./FrontEnd/Form/ClientForm/QuestionPage";
+import TherapistQuestionPage from "./FrontEnd/Form/TherapistForm/QuestionPage";
+import CheckboxQuestionPage from "./FrontEnd/Form/ClientForm/QuestionPageCheckbox";
+import { useState } from "react";
+const ClientQuestions = [
   { question: "What is your Gender identity?", answers: ["Male", "Female"] },
   { question: "What  is the range of your age ?", answers: ["18-25", "26-40","40-55","55-65","above 65"] },
   { question: "What is your relationship status?", answers: ["Single", "In relationship","Married","Divorced","Widowed"] },
@@ -25,8 +29,13 @@ const questions = [
   { question: "How would you rate your current sleeping habits?", answers: ["Good", "Fair","Poor"] },
   { question: "Are you currently experiencing any chronic pain?", answers: ["Yes", "No"] },
 ];
-
+const ClientCheckBoxQuestions = [
+  {questionindex:17, question: "Please Mark that apply all?", answers: ["Im a student"," Im a veteran","Im disabled","Im Unemployed","Im Employed But my income is low",] },
+  { questionindex:18,question: "What led you to consider therapy today?", answers: ["I've been feeling depressed", "I feel anxious or overwhelmed","My mood is interfering with my job/school performance","I struggle with building or maintaining relationships","I am grieving","I can't find purpose and meaning in my life","I have experienced trauma","I need to talk through a specific challenge","I want to gain self confidence","I want to improve myself but I don't know where to start","Recommended to me (friend, family, doctor)"," Just exploring ","Other"] },
+  {questionindex:19, question: "What are your expectations from your therapist? A therapist who...?", answers: ["Listens", "Explores my past","Teaches me new skills","Challenges my beliefs","Assigns me homework","Guides me to set goals","Proactively checks in with me","Others"] }
+];
 function App() {
+
   return (
     <BrowserRouter>
       <Routes>
@@ -34,14 +43,28 @@ function App() {
         <Route path="/TherapistSignup" element={<TherapistSignup />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Verification" element={<Verification />} />
-        <Route path="/FormPage0" element={<WelcomeFrom />} />
-        {questions.map((questionData, index) => (
-      <Route key={index + 1} path={`/FormPage${index + 1}`} element={
-        <QuestionPage
+        <Route path="/ClientWelcomePage" element={<ClientWelcomeForm />} />
+        <Route path="/TherapistWelcomePage" element={<TherapistWelcomeForm />} />
+
+        {ClientQuestions.map((questionData, index) => (
+      <Route key={index + 1} path={`/ClientFormPage${index + 1}`} element={
+        <ClientQuestionPage
           question={questionData.question}
           answers={questionData.answers}
-          backLink={`/FormPage${index}`}
-          nextLink={`/FormPage${index + 2}`}
+          backLink={`/ClientFormPage${index}`}
+          nextLink={`/ClientFormPage${index + 2}`}
+        />}
+      />
+    ))}
+  
+    {ClientCheckBoxQuestions.map((questionData) => (
+  
+      <Route key={questionData.questionindex + 1} path={`/ClientFormPage${questionData.questionindex + 1}`} element={
+        <CheckboxQuestionPage
+          question={questionData.question}
+          answers={questionData.answers}
+          backLink={`/ClientFormPage${questionData.questionindex}`}
+          nextLink={`/ClientFormPage${questionData.questionindex + 2}`}
         />}
       />
     ))}
