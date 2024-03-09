9f8b8d62c660c779bbeecd717dcea78c24365f8e
kal-dereje <kalabdereje20@gmail.com>
2024-03-09 21:14:51 +0300

    chat height

--- src/FrontEnd/Chat/Chat.jsx
+++ src/FrontEnd/Chat/Chat.jsx
@@ -317,7 +317,7 @@ function Chat() {
               </button>
             </div>
           )}
-          <div className="w-full overflow-y-scroll p-4  h-[65%]">
+          <div className="w-full overflow-y-scroll p-4  h-[68%]">
             {receivedMessages.map((msg, index) => {
               if (msg.senderName == userName) {
                 return <SenderMessage key={index} message={msg.message} />;
