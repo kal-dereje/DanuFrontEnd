520aa216d5e9c80ece78b27e3f5eb005612f4e51
kal-dereje <kalabdereje20@gmail.com>
2024-03-13 19:51:54 +0300

    fixed chat issue when there is no one to chat

--- src/FrontEnd/Chat/Chat.jsx
+++ src/FrontEnd/Chat/Chat.jsx
@@ -28,10 +28,18 @@ function Chat() {
   const [showNote, setShowNote] = useState(false);
   const [userID, setUserID] = useState(sessionStorage.getItem("userID"));
   const [userName, setUserName] = useState(sessionStorage.getItem("userName"));
-  const [targetUserID, setTargetUserID] = useState(userList[0]["_id"]);
+  const [targetUserID, setTargetUserID] = useState(() => {
+    if (userList.length == 0) return 0;
+
+    return userList[0]["_id"];
+  });
   const [message, setMessage] = useState("");
   const [receivedMessages, setReceivedMessages] = useState([]);
-  const [currentUser, setCurrentUser] = useState(userList[0]);
+  const [currentUser, setCurrentUser] = useState(() => {
+    if (userList.length == 0) return 0;
+
+    return userList[0];
+  });
 
   const peerInstance = useRef(null);
   const [incomingCall, setIncomingCall] = useState(null);
@@ -228,153 +236,159 @@ function Chat() {
       {sessionStorage.getItem("role") == "therapist" && <Header2 />}
       {sessionStorage.getItem("role") == "admin" && <AdminHeader />}
 
-      <div className="flex  w-full  h-[90vh] relative overflow-y-hidden ">
-        <div className="w-1/4 bg-[#045257] hidden md:block  bg-opacity-80 h-full">
-          {userList.map((user, index) => {
-            return (
-              <div
-                onClick={() => {
-                  handleClickIndex(index);
-                }}
-                key={index}
-                className="border-b-[1px] border-teal-900 py-4 bg-[#F2894E] hover:bg-opacity-90 active:bg-opacity-100   bg-opacity-80 flex items-center  gap-4 justify-center"
-              >
-                <div className="w-[90%]  flex items-center  gap-6  justify-start">
-                  <ProfilePicture
-                    userId={user?._id}
-                    firstName={user?.firstName}
-                  />
-                  <button
-                    onClick={setNoteFalse}
-                    className="flex flex-col justify-start  items-start"
-                  >
-                    <h1 className="text-lg ">{`${user.firstName} ${user.lastName}`}</h1>
-                    <p className=" text-xs text-gray-200">{`${user.email}`}</p>
-                  </button>
-                </div>
-              </div>
-            );
-          })}
+      {userList.length == 0 ? (
+        <div className="w-full h-full bg-[#F2894E] font-semibold text-white py-5 text-lg flex items-start justify-center">
+          No Chat available
         </div>
-        <div className="w-full  h-screen">
-          <div className="flex items-center bg-gray-200 justify-between px-5">
-            <div className="flex gap-5 h-20  items-center">
-              {profilePic == null ? (
-                <div className=" text-xl py-4 px-6 bg-teal-500 rounded-full">
-                  {capitalizeFirstLetter(`${user.firstName}`)}
+      ) : (
+        <div className="flex  w-full  h-[90vh] relative overflow-y-hidden ">
+          <div className="w-1/4 bg-[#045257] hidden md:block  bg-opacity-80 h-full">
+            {userList.map((user, index) => {
+              return (
+                <div
+                  onClick={() => {
+                    handleClickIndex(index);
+                  }}
+                  key={index}
+                  className="border-b-[1px] border-teal-900 py-4 bg-[#F2894E] hover:bg-opacity-90 active:bg-opacity-100   bg-opacity-80 flex items-center  gap-4 justify-center"
+                >
+                  <div className="w-[90%]  flex items-center  gap-6  justify-start">
+                    <ProfilePicture
+                      userId={user?._id}
+                      firstName={user?.firstName}
+                    />
+                    <button
+                      onClick={setNoteFalse}
+                      className="flex flex-col justify-start  items-start"
+                    >
+                      <h1 className="text-lg ">{`${user.firstName} ${user.lastName}`}</h1>
+                      <p className=" text-xs text-gray-200">{`${user.email}`}</p>
+                    </button>
+                  </div>
                 </div>
-              ) : (
-                <img
-                  className="border-neutral-300  text-center text-white h-16 w-16 rounded-full  border-0"
-                  src={profilePic}
-                  alt=" Profile Picture"
-                />
-              )}
-              <div className="flex flex-col">
-                <p>{`${currentUser.firstName} ${currentUser.lastName}`}</p>
-                <p className="text-gray-400">{`${currentUser.email} `}</p>
-              </div>
-            </div>
-
-            <div className="flex  gap-10 px-10">
-              {isPeer == true || perrId != null ? (
-                <p onClick={videoCall}>
-                  <IoVideocam
-                    size={30}
-                    className="text-orange-500 hover:text-teal-600"
-                  />
-                </p>
-              ) : null}
-              {sessionStorage.getItem("role") == "client" && (
-                <p
-                  onClick={setAppointment}
-                  className="px-4 py-1 bg-teal-900 hover:bg-orange-500 text-white rounded"
-                >
-                  Set an Appointment
-                </p>
-              )}
-
-              {sessionStorage.getItem("role") == "therapist" && (
-                <p
-                  onClick={getNotes}
-                  className="px-4 py-1 bg-teal-900 hover:bg-teal-700 active:bg-teal-600 text-white rounded"
-                >
-                  My Notes
-                </p>
-              )}
-            </div>
-          </div>
-          <audio
-            id="ringtone"
-            src="../../src/assets/ringtones.mp3"
-            loop
-          ></audio>
-          {incomingCall && !isPhonePicked && (
-            <div className="mb-4 relative z-50">
-              <p>Incoming call from {incomingCall.peer}</p>
-              <button
-                onClick={handleIncomingCall}
-                className="bg-green-500 text-white p-2 rounded mr-2"
-              >
-                Pick Up
-              </button>
-              <button
-                onClick={rejectIncomingCall}
-                className="bg-red-500 text-white p-2 rounded"
-              >
-                Cancel
-              </button>
-            </div>
-          )}
-          <div className="w-full overflow-y-scroll p-4  h-[68%]">
-            {receivedMessages.map((msg, index) => {
-              if (msg.senderName == userName) {
-                return <SenderMessage key={index} message={msg.message} />;
-              }
-              return <ReciverMessage key={index} message={msg.message} />;
+              );
             })}
           </div>
-          <br />
-          <div className="flex mx-2 items-center justify-between gap-5 ">
-            <textarea
-              className="border-2 rounded-xl  p-2 w-full "
-              rows="1"
-              cols="50"
-              placeholder="Type your message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              onKeyDown={handleKeyPress}
-            />
+          <div className="w-full  h-screen">
+            <div className="flex items-center bg-gray-200 justify-between px-5">
+              <div className="flex gap-5 h-20  items-center">
+                {profilePic == null ? (
+                  <div className=" text-xl py-4 px-6 bg-teal-500 rounded-full">
+                    {capitalizeFirstLetter(`${user.firstName}`)}
+                  </div>
+                ) : (
+                  <img
+                    className="border-neutral-300  text-center text-white h-16 w-16 rounded-full  border-0"
+                    src={profilePic}
+                    alt=" Profile Picture"
+                  />
+                )}
+                <div className="flex flex-col">
+                  <p>{`${currentUser.firstName} ${currentUser.lastName}`}</p>
+                  <p className="text-gray-400">{`${currentUser.email} `}</p>
+                </div>
+              </div>
 
-            <button
-              className="bg-blue-950 text-white p-2 px-5 rounded-lg"
-              onClick={sendMessage}
-            >
-              Send
-            </button>
-          </div>
-          {showNote && (
-            <div className=" absolute left-0 top-56 rounded-lg bg-gray-300 flex  items-center flex-col gap-1  w-[20%] z-30 h-[28rem]">
-              <p className="bg-[#1E232A] text-gray-300 text-xs w-[95%] my-5 px-2 py-2 rounded">
-                Your Note about your Client will be saved you press the Send
-                Button
-              </p>
+              <div className="flex  gap-10 px-10">
+                {isPeer == true || perrId != null ? (
+                  <p onClick={videoCall}>
+                    <IoVideocam
+                      size={30}
+                      className="text-orange-500 hover:text-teal-600"
+                    />
+                  </p>
+                ) : null}
+                {sessionStorage.getItem("role") == "client" && (
+                  <p
+                    onClick={setAppointment}
+                    className="px-4 py-1 bg-teal-900 hover:bg-orange-500 text-white rounded"
+                  >
+                    Set an Appointment
+                  </p>
+                )}
+
+                {sessionStorage.getItem("role") == "therapist" && (
+                  <p
+                    onClick={getNotes}
+                    className="px-4 py-1 bg-teal-900 hover:bg-teal-700 active:bg-teal-600 text-white rounded"
+                  >
+                    My Notes
+                  </p>
+                )}
+              </div>
+            </div>
+            <audio
+              id="ringtone"
+              src="../../src/assets/ringtones.mp3"
+              loop
+            ></audio>
+            {incomingCall && !isPhonePicked && (
+              <div className="mb-4 relative z-50">
+                <p>Incoming call from {incomingCall.peer}</p>
+                <button
+                  onClick={handleIncomingCall}
+                  className="bg-green-500 text-white p-2 rounded mr-2"
+                >
+                  Pick Up
+                </button>
+                <button
+                  onClick={rejectIncomingCall}
+                  className="bg-red-500 text-white p-2 rounded"
+                >
+                  Cancel
+                </button>
+              </div>
+            )}
+            <div className="w-full overflow-y-scroll p-4  h-[68%]">
+              {receivedMessages.map((msg, index) => {
+                if (msg.senderName == userName) {
+                  return <SenderMessage key={index} message={msg.message} />;
+                }
+                return <ReciverMessage key={index} message={msg.message} />;
+              })}
+            </div>
+            <br />
+            <div className="flex mx-2 items-center justify-between gap-5 ">
               <textarea
-                className="bg-[#1E232A] text-gray-200 text-xs w-[96%] mb-3 px-2 py-2 h-[80%] rounded"
-                placeholder="Write here  ..."
-              >
-                {myNote}
-              </textarea>
+                className="border-2 rounded-xl  p-2 w-full "
+                rows="1"
+                cols="50"
+                placeholder="Type your message"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                onKeyDown={handleKeyPress}
+              />
+
               <button
-                className="absolute top-[25rem] left-[15rem]  bg-[#1E232A] hover:text-teal-500 text-gray-200 z-40"
-                onClick={getNotes}
+                className="bg-blue-950 text-white p-2 px-5 rounded-lg"
+                onClick={sendMessage}
               >
-                <VscSend size={25} />
+                Send
               </button>
             </div>
-          )}
+            {showNote && (
+              <div className=" absolute left-0 top-56 rounded-lg bg-gray-300 flex  items-center flex-col gap-1  w-[20%] z-30 h-[28rem]">
+                <p className="bg-[#1E232A] text-gray-300 text-xs w-[95%] my-5 px-2 py-2 rounded">
+                  Your Note about your Client will be saved you press the Send
+                  Button
+                </p>
+                <textarea
+                  className="bg-[#1E232A] text-gray-200 text-xs w-[96%] mb-3 px-2 py-2 h-[80%] rounded"
+                  placeholder="Write here  ..."
+                >
+                  {myNote}
+                </textarea>
+                <button
+                  className="absolute top-[25rem] left-[15rem]  bg-[#1E232A] hover:text-teal-500 text-gray-200 z-40"
+                  onClick={getNotes}
+                >
+                  <VscSend size={25} />
+                </button>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
